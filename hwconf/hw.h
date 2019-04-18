05c60662a5a61b097e142609b63b6c00c56624fd
vadim <vadim@flytrex.com>
2019-04-18 20:54:52 +0300

    added configuration files for FTX board

--- hwconf/hw.h
+++ hwconf/hw.h
@@ -42,6 +42,8 @@
 #include "hw_410.h"
 #elif defined HW_VERSION_60
 #include "hw_60.h"
+#elif defined HW_VERSION_60_FTX
+#include "hw_60_ftx.h"
 #elif defined HW_VERSION_R2
 #include "hw_r2.h"
 #elif defined HW_VERSION_VICTOR_R1A
