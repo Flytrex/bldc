453bafbda6bf68a949783e7134a6d8c70d201c51
vadim <vadim@flytrex.com>
2020-08-17 13:14:00 +0300

    printed CPU ID made consistent with byte ordering

--- applications/app_brake.c
+++ applications/app_brake.c
@@ -196,7 +196,7 @@ static void terminal_cmd_brake_status(int argc, const char **argv) {
     } else if (argc==1) {
         commands_printf("Brake Status");
         commands_printf("   FW version: %s", GIT_VERSION);
-        commands_printf("   HW id: %x %x %x", STM32_UUID[0], STM32_UUID[1], STM32_UUID[2]);
+        commands_printf("   HW id: %08x %08x %08x", __REV(STM32_UUID[0]), __REV(STM32_UUID[1]), __REV(STM32_UUID[2]) );
         commands_printf("   CURRENT_SHUNT_RES: %.1f uOhm", (double)(CURRENT_SHUNT_RES*1e6));
         commands_printf("   App running: %s", is_running ? "On" : "Off");
         commands_printf("   Active: %s", is_active ? "On" : "Off");
